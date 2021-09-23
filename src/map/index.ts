aa04f7e4e8a0907e66db79f4a39b33b3e2d1d2ce
onelikeandidie <onelikeandidie@gmail.com>
2021-09-23 16:54:59 +0100

    Cleanup Just forgot to depricate some stuff

--- src/map/index.ts
+++ src/map/index.ts
@@ -1,8 +1,17 @@
 import DrawingLine from './drawingline';
-import TileGrid from './tilegrid';
-import Tile from './tile';
 import { HexGrid, Hex } from './hexgrid';
 import * as generators from './generators';
 import * as importers from './importers';
+import { Board } from './board';
+import { Entity, ObjectEntity } from './entity';
 
-export { DrawingLine, TileGrid, Tile, HexGrid, Hex, generators, importers };
+export {
+  DrawingLine,
+  HexGrid,
+  Hex,
+  generators,
+  importers,
+  Board,
+  Entity,
+  ObjectEntity
+};
