33fb64b7dd1124defc83a2b4f0a8b7abd0e41c7b
onelikeandidie <onelikeandidie@gmail.com>
2021-09-25 11:09:07 +0100

    Entity has a better constructor

--- src/map/entity.ts
+++ src/map/entity.ts
@@ -21,14 +21,16 @@ class ObjectEntity {
     id: number,
     cloudModelLink: string,
     cloudTextureLink: string,
-    initialPosition?: Vector3
+    initialPosition?: Vector3,
+    initialScale?: Vector3,
+    rotation?: Vector3
   ) {
     this.id = id;
     this.cloudModelLink = cloudModelLink;
     this.cloudTextureLink = cloudTextureLink;
     this.position = initialPosition || Vector3.Zero;
-    this.scale = Vector3.One;
-    this.rotation = Vector3.Zero;
+    this.scale = initialScale || Vector3.One;
+    this.rotation = rotation || Vector3.Zero;
     this.castShadows = false;
     this.receiveShadows = false;
   }
